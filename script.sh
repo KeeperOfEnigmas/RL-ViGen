b1cbda1dda8c1ff0c3c4952ffd1efceebb9bce00
KeeperOfEnigmas <wilsonlin0725@gmail.com>
2025-07-28 13:53:48 +0200

    Update bash script.

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
-easy_task_list=('walker_walk')
+easy_task_list=('walker_walk', 'pendulum_swingup')
+seed_list=(1 3 5)
 frames=1001000
 feature_dim=50
 sgqn_quantile=0.93
@@ -7,20 +8,22 @@ aux_lr=8e-5
 env=dmc
 
 for task_name in ${easy_task_list[@]};
-do
-    CUDA_VISIBLE_DEVICES=0  python train.py \
-                                env=${env} \
-                                task_name=${task_name} \
-                                seed=5 \
-                                action_repeat=${action_repeat} \
-                                use_wandb=False \
-                                use_tb=False \
-                                num_train_frames=${frames} \
-                                save_snapshot=True \
-                                save_video=False \
-                                feature_dim=${feature_dim}
-#                             agent.sgqn_quantile=${sgqn_quantile} \
-#                             agent.aux_lr=${aux_lr}
+    for seed in ${seed_list[@]};
+        do
+            CUDA_VISIBLE_DEVICES=0  python train.py \
+                                        env=${env} \
+                                        task_name=${task_name} \
+                                        seed=${seed} \
+                                        action_repeat=${action_repeat} \
+                                        use_wandb=False \
+                                        use_tb=False \
+                                        num_train_frames=${frames} \
+                                        save_snapshot=True \
+                                        save_video=False \
+                                        feature_dim=${feature_dim} \
+        #                             agent.sgqn_quantile=${sgqn_quantile} \
+        #                             agent.aux_lr=${aux_lr}
+    done
 done
 
 # python train.py env=dmc task_name='walker_walk' seed=5 action_repeat=2 use_wandb=False use_tb=False num_train_frames=1001000 save_snapshot=True save_video=False feature_dim=50
