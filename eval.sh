c9f156bedf60ff3f70eac8352468bce42865de87
KeeperOfEnigmas <wilsonlin0725@gmail.com>
2026-02-17 20:36:27 +0100

    Modify eval.sh and script.sh

--- eval.sh
+++ eval.sh
@@ -1,61 +1,95 @@
 #!/bin/bash
-
-env=dmc
-task_list=(pendulum_swingup)
-algorithm=(svea)
-seed_list=(1 2 3 4 5)
-# seed_list=(1)
-augmentation=("cutmix" "cutout" "no_aug" "overlay" "cropping" "window" "rotation" "flip_v" "flip_h" "convolution" "mix")
-# augmentation=("cutmix")
-eval_augmentation=("vignette" "distortion" "cutmix" "cutout" "overlay" "cropping" "window" "rotation" "flip_h" "flip_v" "convolution")
-eval_type=("original" "color" "video")
-# eval_type=("original")
-eval_difficulty=("easy" "hard")
+export MUJOCO_GL=glfw
+# env=dmc
+env=robosuite
+# task_list=(Door)
+task_list=(TwoArmLift)
+algorithm=(pieg)
+# seed_list=(1 2 3 4 5)
+# seed_list=(5)
+seed_list=(1)
+# augmentation=("cutmix" "cutout" "no_aug" "overlay" "cropping" "window" "rotation" "flip_v" "flip_h" "convolution" "mix")
+# augmentation=("cutmix" "cutout" "no_aug" "cropping" "window")
+# augmentation=("rotation" "flip_v" "flip_h" "convolution" "mix")
+augmentation=("no_aug")
+eval_augmentation=("no_aug")
+# eval_augmentation=("vignette" "distortion" "cutmix" "cutout" "overlay" "cropping" "window" "rotation" "flip_h" "flip_v" "convolution")
+# eval_type=("original" "video" "color")
+# eval_type=("video" "color")
+eval_type=("original")
+# eval_difficulty=("easy" "hard")
+eval_difficulty=("easy")
+mode=("eval-easy" "eval-medium" "eval-hard")
+mode=("train")
 
 for algo in "${algorithm[@]}"; do
     for task in "${task_list[@]}"; do
         for seed in "${seed_list[@]}"; do
             for aug in "${augmentation[@]}"; do
                 for type in "${eval_type[@]}"; do
-                    if  [ "$type" == "original" ]; then
-                        for eval_aug in "${eval_augmentation[@]}"; do
-                            model_path="/home/weiyi/RL-ViGen/exp_local/${algo}/${task}/${seed}/${aug}/snapshot.pt"
-                            CUDA_VISIBLE_DEVICES=0 python locodmc_eval.py \
-                                                            env=${env} \
-                                                            task=${task} \
-                                                            seed=${seed} \
-                                                            action_repeat=2 \
-                                                            use_wandb=False \
-                                                            use_tb=False \
-                                                            save_snapshot=False \
-                                                            save_video=True \
-                                                            wandb_group=walker_walk \
-                                                            +eval_aug=${eval_aug} \
-                                                            model_dir=${model_path} \
-                                                            +eval_type=original \
-                                                            +eval_difficulty=easy \
-                                                            +aug=${aug} \
-                        
-                        done
+                    if [ "$env" == "dmc" ]; then
+                        if  [ "$type" == "original" ]; then
+                            for eval_aug in "${eval_augmentation[@]}"; do
+                                # model_path="/home/weiyi/RL-ViGen/exp_local/${algo}/${task}/${seed}/${aug}/snapshot.pt"
+                                CUDA_VISIBLE_DEVICES=0 python locodmc_eval.py \
+                                                                env=${env} \
+                                                                task=${task} \
+                                                                seed=${seed} \
+                                                                action_repeat=2 \
+                                                                use_tb=False \
+                                                                save_snapshot=False \
+                                                                +eval_aug=${eval_aug} \
+                                                                +aug=${aug} \
+                                                                wandb_group=walker_walk \
+                                                                +mode=eval-hard \
+                                                                # save_video=True \
+                                                                # use_wandb=False \
+                                                                # +eval_type=original \
+                                                                # +eval_difficulty=easy \
+                                                                # model_dir=${model_path} \
+                                                                # +aug=${aug} \
+                            
+                            done
+                        else
+                            for difficulty in "${eval_difficulty[@]}"; do
+                                # model_path="/home/weiyi/RL-ViGen/exp_local/${algo}/${task}/${seed}/${aug}/snapshot.pt"
+                                CUDA_VISIBLE_DEVICES=0 python locodmc_eval.py \
+                                                                env=${env} \
+                                                                task=${task} \
+                                                                seed=${seed} \
+                                                                action_repeat=2 \
+                                                                use_tb=False \
+                                                                save_snapshot=False \
+                                                                wandb_group=walker_walk \
+                                                                eval_type=${type} \
+                                                                eval_difficulty=${difficulty} \
+                                                                +eval_aug=${type}_${difficulty} \
+                                                                +aug=${aug} \
+                                                                # save_video=True \
+                                                                # use_wandb=False \
+                                                                # +eval_type=${type} \
+                                                                # +eval_difficulty=${difficulty} \
+                                                                # model_dir=${model_path} \
+                                                                # +aug=${aug} \
+                            
+                            done
+                        fi
                     else
-                        for difficulty in "${eval_difficulty[@]}"; do
-                            model_path="/${algo}/${task}/${seed}/${aug}/snapshot.pt"
-                            CUDA_VISIBLE_DEVICES=0 python locodmc_eval.py \
-                                                            env=${env} \
-                                                            task=${task} \
-                                                            seed=${seed} \
-                                                            action_repeat=2 \
-                                                            use_wandb=False \
-                                                            use_tb=False \
-                                                            save_snapshot=False \
-                                                            save_video=True \
-                                                            wandb_group=walker_walk \
-                                                            model_dir=${model_path} \
-                                                            +eval_type=${type} \
-                                                            +eval_difficulty=${difficulty} \
-                                                            +aug=${aug} \
-                        
-                        done
+                        for m in "${mode[@]}"; do
+                                # model_path="/home/weiyi/RL-ViGen/exp_local/${algo}/${task}/${seed}/${aug}/snapshot.pt"
+                                CUDA_VISIBLE_DEVICES=0 python locodmc_eval.py \
+                                                                env=${env} \
+                                                                task=${task} \
+                                                                seed=${seed} \
+                                                                action_repeat=2 \
+                                                                use_tb=False \
+                                                                save_snapshot=False \
+                                                                wandb_group=walker_walk \
+                                                                +eval_aug=no_aug \
+                                                                +aug=${aug} \
+                                                                +mode=${m} \
+
+                            done
                     fi
                 done
             done
@@ -63,4 +97,4 @@ for algo in "${algorithm[@]}"; do
     done
 done
 
-
+# redo pieg/pendulum_swingup/1, 2, 3, all evaluation augmentations
--- script.sh
+++ script.sh
@@ -1,17 +1,24 @@
-easy_task_list=('pendulum_swingup' 'cheetah_run' 'humanoid_walk')
-seed_list=(1 2 3 4 5)
+# export MUJOCO_GL=glfw
+# easy_task_list=('pendulum_swingup' 'cheetah_run' 'humanoid_walk' 'Door' 'Lift')
+easy_task_list=('pendulum_swingup')
+# easy_task_list=('Door')
+# seed_list=(3)
+seed_list=(4)
 # seed_list=(2 3 4 5)
-algorithm=("svea" "pieg")
+# algorithm=("svea" "pieg")
+algorithm=("pieg")
 # eval_type=("video" "color")
 # eval_difficulty=("hard" "easy")
-#augmentation=("overlay" "cropping" "window" "rotation" "flip_v" "flip_h" "convolution" "cutout" "cutmix" "no_aug") 
-augmentation=("cutmix" "cutout" "no_aug" "overlay" "cropping" "window" "rotation" "flip_v" "flip_h" "convolution" "mix") 
+augmentation=("no_aug") 
+# augmentation=("window" "flip_v" "flip_h") 
+# augmentation=("cutmix" "cutout" "no_aug" "overlay" "cropping" "window" "rotation" "flip_v" "flip_h" "convolution" "mix") 
 frames=1001000
 feature_dim=50
 sgqn_quantile=0.93
 action_repeat=2
 aux_lr=8e-5
 env=dmc
+# env=robosuite
 
 for algo in ${algorithm[@]}; do
     for task_name in ${easy_task_list[@]}; do
@@ -24,14 +31,14 @@ for algo in ${algorithm[@]}; do
                                                     task_name=${task_name} \
                                                     seed=${seed} \
                                                     action_repeat=${action_repeat} \
-                                                    use_wandb=False \
-                                                    use_tb=False \
                                                     num_train_frames=${frames} \
-                                                    save_snapshot=True \
-                                                    save_video=False \
                                                     feature_dim=${feature_dim} \
                                                     agent._target_=algos.${algo}.${algo^^}Agent \
                                                     +aug=${aug} \
+                                                    # use_wandb=False \
+                                                    # use_tb=False \
+                                                    # save_snapshot=True \
+                                                    # save_video=False \
                                                     # eval_type=${type} \
                                                     # eval_difficulty=${difficulty} \
                     #                             agent.sgqn_quantile=${sgqn_quantile} \
